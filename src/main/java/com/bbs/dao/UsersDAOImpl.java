5f4b8623d1a7b0383344b03e41c6ebb791e716e3
jayyouu <duwls5513@naver.com>
2021-11-25 12:53:01 +0900

    2021-11-25 12:52 이메일 인증  commit

--- src/main/java/com/bbs/dao/UsersDAOImpl.java
+++ src/main/java/com/bbs/dao/UsersDAOImpl.java
@@ -5,16 +5,36 @@ import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import com.bbs.vo.Authmail;
+
 @Repository	// DAO 기능을 한다는 것을 추가시킴
 public class UsersDAOImpl implements UsersDAO {
 	
 	@Inject
 	SqlSession sqlSession;
 	
+	final String SESSION = "com.bbs.mappers.bbs";
+	
 	@Override
 	public String idCheck(String user_id) throws Exception {
 								// 처음 전달할 문자 : namespace 이름.실행할 구문 id 이름, 받아올데이터) 
-		return sqlSession.selectOne("com.bbs.mappers.bbs.idCheck", user_id);
+		return sqlSession.selectOne(SESSION + ".idCheck", user_id);
+	}
+	
+	@Override
+	public Integer getAuthnum(String user_mail) throws Exception {
+		return sqlSession.selectOne(SESSION + ".getAuthnum", user_mail);
+	}
+	
+	@Override
+	public void setAuthnum(Authmail authmail) throws Exception {
+		// 반환 타입 void라 리턴 작업 필요 없음
+		sqlSession.insert(SESSION + ".setAuthnum", authmail);
+	}
+
+	@Override
+	public void resetAuthnum(Authmail authmail) throws Exception {
+		sqlSession.update(SESSION + ".resetAuthnum", authmail);
 	}
 
 	
